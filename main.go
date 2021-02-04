453c7c05873d30e14133e9ff5db2cd2d949aeffd
William Vandebelt <wvandebelt@salesforce.com>
2021-02-04 12:13:06 -0500

    :sparkles: clean up

--- main.go
+++ main.go
@@ -1,41 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"text/template"
 
-	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
-
-func getTemplate(fm template.FuncMap, name string) *template.Template {
-	tpl := template.Must(template.New("").Funcs(fm).ParseFiles("templates/"+name+".tmpl.html",
-		"templates/header.tmpl.html", "templates/footer.tmpl.html"))
-	log.Printf("%s template returned\n", name)
-	return tpl
-}
-
-// HandleError : Func that sends back a 500 error code if there is an error
-func handleError(w http.ResponseWriter, err error) {
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println(err)
-	}
-}
-
-func routes() *chi.Mux {
-	router := chi.NewRouter()
-
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		tpl := getTemplate(template.FuncMap{}, "index")
-		err := tpl.ExecuteTemplate(w, "index.tmpl.html", "Hello Message From Index.go")
-		handleError(w, err)
-	})
-
-	return router
-}
 
 func init() {
 	err := godotenv.Load()
@@ -48,8 +20,10 @@ func init() {
 func main() {
 	port := os.Getenv("PORT")
 
-	r := routes()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World\n")
+	})
 
 	log.Printf("Golang App running...\n")
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
